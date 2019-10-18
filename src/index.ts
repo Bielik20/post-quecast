8af558ecef7839f4b35adce3b88b75376e5249d1
Damian Bielecki <bielik20@gmail.com>
2019-10-18 11:00:09 +0200

    chore: add communicator

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
 export function printHello(): void {
   console.log('hello from lib');
 }
+
+export class Communicator {
+  private connections = new Set<Window>();
+  private history: any[] = [];
+
+  constructor(private instanceName: string) {
+    this.connections.add(window);
+    this.listen();
+  }
+
+  connect(): void {
+    window.top.postMessage({ type: 'connect' }, '*');
+  }
+
+  listen(): void {
+    window.addEventListener(
+      'message',
+      event => {
+        console.log(this.instanceName, event);
+        if (event.data.type === 'connect') {
+          const connection = event.source as Window;
+
+          this.connections.add(connection);
+          this.history.push(event.data);
+
+          connection.postMessage(
+            { type: 'connected', history: this.history, connections: this.connections },
+            '*',
+          );
+        }
+      },
+      false,
+    );
+  }
+}
