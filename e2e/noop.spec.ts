4d53e203c044bae2bf5d9f0acd01e51b7d66d0c2
Damian Bielecki <bielik20@gmail.com>
2019-10-23 16:55:40 +0200

    chore: add spec paths

--- e2e/noop.spec.ts
+++ e2e/noop.spec.ts
@@ -1,5 +1,8 @@
+import { setupPostQuecast } from '@wikia/post-quecast';
+
 describe('noop', () => {
   it('is always ok', () => {
+    setupPostQuecast();
     expect(true).toBe(true);
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.spec.json',
+    },
+  },
+  moduleNameMapper: {
+    '^@wikia/post-quecast': '<rootDir>/src/index.ts',
+  },
 };
